b48eb0ff4e1fdf877f2db4ac0ec2ba3d0137f4b1
Jake Karl <jakekarl3@gmail.com>
2018-06-13 02:24:20 +0000

    Messing with pagination CSS

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20180605013621) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false

7ebc7c3545d48a9710dbf07e3eb6b8e2b8ea96e3
Jake Karl <jakekarl3@gmail.com>
2018-06-26 03:18:54 +0000

    Fixed API keys

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "jakekarl3@gmail.com",
+    mail(to: "jakekarldevelopment@gmail.com",
         subject: "A comment has been added to your place")
   end
 end
